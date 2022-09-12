c62ba401d1a7b1c2d76503e8ee397a410d57d830
naufal.fikri11 <naufal.fikri11@ui.ac.id>
2022-09-12 10:54:49 +0700

    final commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(req):
+    data_katalog = CatalogItem.objects.all()
+    ctx = {
+        'list_katalog':  data_katalog,
+        'nama': 'Naufal Fikri Hakim',
+        'id': '2106750566'
+
+    }
+
+    return render(req, "katalog.html", ctx)

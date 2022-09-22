1c5fe9b31906256de64ac2b29051acfe3ef9cf30
naufal.fikri11 <naufal.fikri11@ui.ac.id>
2022-09-22 11:36:33 +0700

    fixing models.py

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,7 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class MyWatchList(models.Model):
     watched = models.BooleanField(default=False)
     title = models.TextField()
-    # create rating with minimum 1 to 5
     rating = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
     release_date = models.TextField()
     review = models.TextField()
